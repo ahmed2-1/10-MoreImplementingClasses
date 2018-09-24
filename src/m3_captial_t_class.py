77310de599511e1a8341461057d6d595d6ffe0bb
ahmed1 <ahmed1@rose-hulman.edu>
2018-09-24 11:01:45 -0400

    dfgn

--- src/m3_captial_t_class.py
+++ src/m3_captial_t_class.py
@@ -2,8 +2,8 @@
 A   CapitalT   class and methods that use the Cross class.
 
 Authors: David Mutchler, Vibha Alangar, Dave Fisher, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Tom Ahmed.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -22,7 +22,7 @@ def main():
     # run_test_simple_t()
     # run_test_set_colors()
     # run_test_move_by()
-    # run_test_clone()
+    run_test_clone()
 
 
 def run_test_simple_t():
@@ -146,11 +146,13 @@ class CapitalT(object):
           :type letter_thickness:   int
         """
         # --------------------------------------------------------------
-        # TODO: 3.
+        # DONE: 3.
         #   READ the above specification, including the Example.
         #   Implement this method
         #   Note: you will need to also implement attach_to before testing
         # --------------------------------------------------------------
+        self.h_rect = rg.Rectangle(rg.Point((intersection_center.x-width/2), (intersection_center.y+letter_thickness/2)), rg.Point((intersection_center.x+width/2), (intersection_center.y-letter_thickness/2)))
+        self.v_rect = rg.Rectangle(rg.Point((intersection_center.x-letter_thickness/2), (intersection_center.y-letter_thickness/2)), rg.Point((intersection_center.x+letter_thickness/2), (intersection_center.y - letter_thickness/2 + height)))
 
     def attach_to(self, window):
         """
@@ -171,11 +173,13 @@ class CapitalT(object):
           :type window: rg.RoseWindow
         """
         # --------------------------------------------------------------
-        # TODO: 4.
+        # DONE: 4.
         #   READ the above specification, including the Example.
         #   Implement and test this method by looking at the console and
         #     the graphics window (compare it to simple_t.pdf)
         # --------------------------------------------------------------
+        self.h_rect.attach_to(window)
+        self.v_rect.attach_to(window)
 
     def set_colors(self, fill_color, outline_color):
         """
@@ -198,12 +202,16 @@ class CapitalT(object):
           :type outline_color: str
         """
         # --------------------------------------------------------------
-        # TODO: 5.
+        # DONE: 5.
         #   READ the above specification, including the Example.
         #   Implement and test this method by uncommenting the appropriate
         #     run_test method in main. Compare the graphics window to
         #     set_colors.pdf.
         # --------------------------------------------------------------
+        self.h_rect.outline_color = outline_color
+        self.v_rect.outline_color = outline_color
+        self.h_rect.fill_color = fill_color
+        self.v_rect.fill_color = fill_color
 
     def move_by(self, dx, dy):
         """
@@ -228,13 +236,15 @@ class CapitalT(object):
           :type dy: int
         """
         # --------------------------------------------------------------
-        # TODO: 6.
+        # DONE: 6.
         #   READ the above specification, including the Example.
         #   Implement and test this method by uncommenting the appropriate
         #     run_test method in main. Compare the graphics window to
         #     move_by.pdf. Note: the pdf shows the different locations
         #     that the T moves through, but there is only 1 T at any moment.
         # --------------------------------------------------------------
+        self.h_rect.move_by(dx, dy)
+        self.v_rect.move_by(dx, dy)
 
     def clone(self):
         """
@@ -262,11 +272,18 @@ class CapitalT(object):
         #     run_test method in main. Compare the graphics window to
         #     clone.pdf.
         # --------------------------------------------------------------
-
-
+        intersection_center = self.h_rect.get_center()
+        width = (self.h_rect.get_upper_right_corner().x-self.h_rect.get_upper_left_corner().x)
+        height = (self.v_rect.get_lower_right_corner().y-self.v_rect.get_upper_right_corner().y)
+        letter_thickness = (self.v_rect.get_upper_right_corner().x-self.v_rect.get_upper_left_corner().x)
+        t = CapitalT(intersection_center, width, height, letter_thickness)
+        t.set_colors(self.h_rect.fill_color, self.h_rect.outline_color)
+        return t
 # ----------------------------------------------------------------------
 # If this module is running at the top level (as opposed to being
 # imported by another module), then call the 'main' function.
 # ----------------------------------------------------------------------
+
+
 if __name__ == '__main__':
     main()
